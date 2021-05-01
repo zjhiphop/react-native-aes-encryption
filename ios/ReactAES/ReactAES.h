c7c48e5c03ff8ebeca543f483c01d3a19c88b002
Jade <x@oliverwyman.com>
2021-05-01 22:11:12 +0800

    add cocoapods support and update package config

--- ios/ReactAES/ReactAES.h
+++ ios/ReactAES/ReactAES.h
@@ -7,7 +7,11 @@
 //
 
 #import <Foundation/Foundation.h>
+#if __has_include(<React/RCTBridgeModule.h>) // React Native >= 0.40
+#import <React/RCTBridgeModule.h>
+#else // React Native < 0.40
 #import "RCTBridgeModule.h"
+#endif
 
 
 @interface ReactAES : NSObject <RCTBridgeModule>
